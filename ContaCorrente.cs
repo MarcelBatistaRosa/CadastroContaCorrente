4d153e4e491347d3613adc7b31501a42e6d28c70
Marcel Batista Rosa <mba.marcel@gmail.com>
2024-08-21 23:09:42 -0300

    Primeira versao do projeto CadastroContaCorrente

---
+++ ContaCorrente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroContaCorrente
+{
+    internal class ContaCorrente
+    {
+        private double Saldo;
+        public int  Conta { get; private set; }
+        public string Nome { get; set; }
+
+        public ContaCorrente (string nome, int conta, double saldo)
+        {
+            Nome = nome;
+            Conta = conta;
+            Saldo = saldo;
+        }
+
+        public ContaCorrente (string nome, int conta)
+        {
+            Nome = nome;
+            Conta = conta;
+            Saldo = 0;
+        }
+
+        public void Deposito (double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo -= saque;
+            Saldo -= 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + Conta.ToString() + ", " + "Titular: " + Nome + ", " + "Saldo: $" + Saldo.ToString("F2");
+        }
+    }
+}   
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using CadastroContaCorrente;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace CadastroContaCorrente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.Write("Entre com o numero da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string nome = Console.ReadLine();
+            
+
+            ContaCorrente Cc = new ContaCorrente(nome, conta);
+            
+
+            Console.Write("Haverá depósito inicial (s/n): ");
+            string Di = Console.ReadLine();
+            if (Di == "s")
+            {
+                Console.Write("Entre com o valor do depósito incial: ");
+                double vdi = double.Parse(Console.ReadLine());
+                Cc.Deposito(vdi);
+                Console.WriteLine();
+                Console.WriteLine("Dados da Conta: ");
+                Console.WriteLine(Cc);
+            } 
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Dados da Conta: ");
+                Console.WriteLine(Cc);
+            }
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor para depósito: ");
+            double D = double.Parse(Console.ReadLine());
+            Cc.Deposito(D);
+            Console.WriteLine(Cc);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor para Saque: ");
+            double S = double.Parse(Console.ReadLine());
+            Cc.Saque(S);
+            Console.WriteLine(Cc);
+
+        }
+    }
+}
